010034c584195bcbeac76120c79cdefd86dee45c
Justin Swiger <justin.swiger@me.com>
2019-10-02 20:24:22 -0400

    changed background color

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+	if (document.querySelector('h1.hello-world')) {
+		document.querySelector('h1.hello-world').textContent = 'Hello, World!';
+		document.querySelector('body').style.background = 'blue';
+	}
+};
 
-document.addEventListener('DOMContentLoaded', main)
+document.addEventListener('DOMContentLoaded', main);
